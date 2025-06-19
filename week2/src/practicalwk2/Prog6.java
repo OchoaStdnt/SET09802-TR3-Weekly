173774760fb52b156232471cfbbcfd04f412b3ab
Ochoa-88 <157509216+Ochoa-88@users.noreply.github.com>
2025-06-19 16:22:28 -0600

    updated prog6

--- week2/src/practicalwk2/Prog6.java
+++ week2/src/practicalwk2/Prog6.java
@@ -7,11 +7,14 @@ public class Prog6 {
 
 	public static void main(String[] args) {
 		// variables
-		int genValue;
-		int maxValue = 0;
-		int minValue = 0;
+		int genValue, maxValue, minValue;
 		String output = "";
 		Random numGenerator = new Random();
+		
+		//generate started values for min and max
+		genValue = numGenerator.nextInt(100);
+		maxValue = genValue;
+		minValue = genValue;
 		
 		for(int count= 0; count < 10; count++) {
 			genValue = numGenerator.nextInt(100);
@@ -19,9 +22,22 @@ public class Prog6 {
 			output = output + "Number: " +genValue +"\n";
 			
 			
-			
+			if(genValue > maxValue) {
+				maxValue = genValue;
+			} else if(genValue < minValue) {
+				minValue = genValue;
 			}
+			
+			/*
+			if(genValue <= minValue) {
+				minValue = genValue;
+			} else if (genValue >= maxValue) {
+				maxValue = genValue;
+			}
+			*/
 		}
+		output = output + "The largest number from the list is : " +maxValue +"\n"
+				+ "The smallest number from the list : " +minValue;
 		
 		//display out
 		JOptionPane.showMessageDialog(null, output, "", JOptionPane.INFORMATION_MESSAGE);
