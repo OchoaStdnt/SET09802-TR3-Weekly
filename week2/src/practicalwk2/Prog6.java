6cd573c486b3033aec0a31913069d2a0b8d222cd
Ochoa-88 <157509216+Ochoa-88@users.noreply.github.com>
2025-06-19 13:16:13 -0600

    prog6 start

---
+++ week2/src/practicalwk2/Prog6.java
@@ -0,0 +1,34 @@
+package practicalwk2;
+
+import javax.swing.JOptionPane;
+import java.util.Random;
+
+public class Prog6 {
+
+	public static void main(String[] args) {
+		// variables
+		int genValue;
+		int maxValue = 0;
+		int minValue = 0;
+		String output = "";
+		Random numGenerator = new Random();
+		
+		for(int count= 0; count < 10; count++) {
+			genValue = numGenerator.nextInt(100);
+			
+			output = output + "Number: " +genValue +"\n";
+			
+			if(genValue >= minValue) {
+				if(genValue >= maxValue) {
+					maxValue = genValue;
+				} else if (genValue <= minValue){
+					minValue = genValue;
+				}
+			}
+		}
+		
+		//display out
+		JOptionPane.showMessageDialog(null, output, "", JOptionPane.INFORMATION_MESSAGE);
+	}
+
+}
