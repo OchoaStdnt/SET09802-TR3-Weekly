991d3b1c32e0bb04031023bfbb74b97de1e3b16e
Ochoa-88 <157509216+Ochoa-88@users.noreply.github.com>
2025-06-18 22:39:19 -0600

    prog 3 completed

--- week2/src/practicalwk2/Prog3.java
+++ week2/src/practicalwk2/Prog3.java
@@ -1,10 +1,26 @@
 package practicalwk2;
 
+import javax.swing.JOptionPane;
+
 public class Prog3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// variables
+		String output, charAsString;
+		char theChar;
+		
+		charAsString = JOptionPane.showInputDialog("please enter a character");
+		theChar = charAsString.charAt(0);
+		
+		//construct output based on the character entered.		
+		if(theChar == 'a' || theChar == 'e' || theChar == 'i' || theChar == 'o' || theChar == 'u' || theChar == 'A' || theChar == 'E' || theChar == 'I' || theChar == 'O' || theChar == 'U') {
+			output = theChar +" is a vowel";
+		} else {
+			output = theChar +" is not a vowel";
+		}
 
+		//print message
+		JOptionPane.showMessageDialog(null, output, "", JOptionPane.PLAIN_MESSAGE);
 	}
 
 }
