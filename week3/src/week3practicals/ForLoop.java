95e3a715d2ff78805545765c57808ab652f48ddc
Ochoa-88 <157509216+Ochoa-88@users.noreply.github.com>
2025-06-21 23:04:47 -0600

    for

---
+++ week3/src/week3practicals/ForLoop.java
@@ -0,0 +1,17 @@
+package week3practicals;
+
+import javax.swing.JOptionPane;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		// variables
+		String output = "";
+		
+		for(int counter = 0; counter <= 10; counter++) {
+			output = output + counter +"\n";
+		}
+		JOptionPane.showMessageDialog(null, output, "", JOptionPane.INFORMATION_MESSAGE);
+	}
+
+}
