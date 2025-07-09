935834e4006ba28ffde4b3e14baab01d96716435
ochoa <ochoa@localhost>
2025-07-09 15:47:25 -0600

    finished question 9

--- week5practice/src/question9/Q9Methods.java
+++ week5practice/src/question9/Q9Methods.java
@@ -1,5 +1,82 @@
 package question9;
 
-public class Q9Methods {
+import javax.swing.JOptionPane;
 
-}
+public class Q9Methods {
+	
+	
+	
+	
+	
+	
+	
+	public static void welcomeMessage() { //method 1
+		
+		String welcomeMessage = "Hello how are you?";
+		JOptionPane.showMessageDialog(null, welcomeMessage, "Hello", JOptionPane.INFORMATION_MESSAGE);
+	
+	}//end welcomeMessage()
+	
+	
+	
+	
+	
+	
+	
+	
+	public static int getMark(int minMark, int maxMark) { //method two
+		//variables
+		String message, theMark;
+		int mark;
+		
+		//do {
+			//read in marks
+			message = "please enter a mark";
+			theMark = JOptionPane.showInputDialog(message);
+	   	   	mark = Integer.parseInt(theMark);
+
+	   	   	//validate mark
+	   	   	while (mark < minMark || mark  > maxMark) {
+		   	 	message = "Please enter a mark between " +minMark +" and " +maxMark;
+				theMark = JOptionPane.showInputDialog(message);
+		   	   	mark = Integer.parseInt(theMark);
+	   	   	}
+	   	  // 	numberOfStudents--;
+		   	//}while(numberOfStudents != 0);
+		return mark;
+		
+	}//end getMark()
+	
+	
+	
+	
+	
+	
+	
+	//the passing array on this method must not have the same name as the array its referencing!!!
+	public static void passFailCheck(int stndtMark, int passMark, int[] passFailCountsRef) { //method 3
+		//int[] passFailResults = new int[2];  //passes will be stored on address 0 and fails on address 1
+		
+		if (stndtMark >= passMark) {
+			passFailCountsRef[0]++;
+		}else {
+			passFailCountsRef[1]++;
+		}
+		
+		//return passFailResults;
+	}// end passFailcheck()
+	
+	
+	
+	
+	
+	
+	
+	public static void outputResults(int passes, int fails) { //method 4
+		String output;
+		output = "The number of passes is " +passes +"\n";
+		output = output  + "The number of fails is " +fails;
+		JOptionPane.showMessageDialog(null, output, "Hello", JOptionPane.INFORMATION_MESSAGE);
+	}
+	
+}//end class
--- week5practice/src/question9/Question9.java
+++ week5practice/src/question9/Question9.java
@@ -1,5 +1,5 @@
 /*
- * Take the class Q9Code in this same package and redo using methods
+ * Take the class Q9Code in this same package and redo using methods, you should use at least four methods.
  */
 
 package question9;
@@ -7,8 +7,37 @@ package question9;
 public class Question9 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// CODE SAMPLE BELOW
+		//variables
+		int numberOfStudents = 3;
+		int minMark = 0;
+		int maxMark = 100;
+		int passMark = 10;
+		int mark;
+		int[] passFailCounts = new int[2]; //passes will be stored on address 0 and fails on address 1
+		
+		
+		//call method 1
+		Q9Methods.welcomeMessage();
+		
+		
+		//loop to get marks, validad and tally fail and passes
+		do {
+		//call method 2
+		mark = Q9Methods.getMark(minMark, maxMark);
+		
+		//Call method 3
+		Q9Methods.passFailCheck(mark, passMark, passFailCounts);
+		numberOfStudents--;
+		}while (numberOfStudents != 0);
+		
+		//call method 4
+		Q9Methods.outputResults(passFailCounts[0], passFailCounts[1]);
+		
+		
 
-	}
+		
 
-}
+	}//end class
+
+}//end main
