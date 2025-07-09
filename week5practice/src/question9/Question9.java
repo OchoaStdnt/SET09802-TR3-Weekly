2d337330aaf316ca83111eb902da60fed4c24568
ochoa <ochoa@localhost>
2025-07-09 00:08:25 -0600

    started question 9

---
+++ week5practice/src/question9/Question9.java
@@ -0,0 +1,14 @@
+/*
+ * Take the class Q9Code in this same package and redo using methods
+ */
+
+package question9;
+
+public class Question9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
