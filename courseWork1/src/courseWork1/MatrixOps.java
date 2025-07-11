d7f5e7d17a1cc981da61a64ff38c2a59c5f2fb2b
ochoa <ochoa@localhost>
2025-07-11 12:58:17 -0600

    fixed method to generate random numbers based on odd and even rows

--- courseWork1/src/courseWork1/MatrixOps.java
+++ courseWork1/src/courseWork1/MatrixOps.java
@@ -69,12 +69,22 @@ public class MatrixOps {
 		//initiate Random generator
 		Random GenNum = new Random();
 		
-		//populate array with random numbers
+		//populate array with random numbers. odd rows will have only odd numbers and even rows will have only even numbers
 		for(int rLoop = 0; rLoop < array.length; rLoop++) {
 			
 			for(int cLoop = 0; cLoop < array[rLoop].length; cLoop++) {
 				//variable to store random number
-				int rNum = GenNum.nextInt(1000); //generate a random number between 0-1000
+				int rNum;
+				
+				//check if row is even or odd
+				if (rLoop % 2 == 0) { //checks for even numbers. anything that is divided by 2 and gives a reminder of 0 is an even number
+					rNum = GenNum.nextInt(501) * 2; //this generates even Numbers
+					
+				}else {
+					rNum = GenNum.nextInt(500) * 2 + 1; //this generates odd Numbers
+				}
+				
+				//= GenNum.nextInt(1000); //generate a random number between 0-1000
 				array[rLoop][cLoop] = rNum;
 			}//end second for loop
 			
